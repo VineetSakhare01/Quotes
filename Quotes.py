3ccc070f970e7846f88e4c8f614579aaa36c135c
Vineet Sakhare <Vineetsakhare01@gmail.com>
2024-10-18 19:38:01 +0530

    Add files via upload

---
+++ Quotes.py
@@ -0,0 +1,14 @@
+print('hello world')
+
+# Q1. Write a python program that prints the following text exactly as appears:
+
+# Python is fun.
+# "Quotes" and 'single quotes' can be tricky
+
+a = "Python is fun."
+b = '''"Quotes" and 'single quotes' can be tricky'''
+
+print(a + b)
+
+
+
